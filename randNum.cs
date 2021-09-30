dba225707ffd9e1b026194b7e9001a83aa7aa3ed
DhruvMitna19 <dhruvmitna9@gmail.com>
2021-09-30 11:21:33 +0530

    added comments

--- randNum.cs
+++ randNum.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// The default code.
+using System;
 
 namespace randNum {
 
@@ -8,8 +9,10 @@ namespace randNum {
 
             try {
 
+                // Initializing the random class.
                 Random rnd = new Random();
 
+                // Getting the user input.
                 Console.Write("Enter the starting number for the range you want the random number to be selected from: ");
                 string start = Console.ReadLine();
                 int Start = int.Parse(start);
@@ -18,18 +21,22 @@ namespace randNum {
                 string end = Console.ReadLine();
                 int End = int.Parse(end);
 
-                if (Start < End) Console.WriteLine("The random number is " + rnd.Next(Start, End) + ".");
+                // Output.
+                if (Start < End) Console.WriteLine($"The random number is {rnd.Next(Start, End)}." + rnd.Next(Start, End) + ".");
 
-                else if (Start > End) Console.WriteLine("The random number is " + rnd.Next(End, Start) + ".");
+                else if (Start > End) Console.WriteLine($"The random number is {rnd.Next(End, Start)}.");
 
+                // Making sure the starting and ending numbers of the range are different.
                 else Console.WriteLine("The starting number and the ending number cannot be equal.");
 
+            // If the user enters a strings instead of an integer, this message is printed out instead of the program crashing.
             } catch (FormatException) {
                 
                 Console.WriteLine("Please enter a valid value");
                 
             }
 
+            // Preventing the program from terminating immediately.
             Console.Write("\nPress enter to exit...");
             Console.ReadLine();
 
